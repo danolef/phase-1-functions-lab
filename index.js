d8141c87adf20f7654c3304ad85fcbbfa6d94231
Dan <dt.lefever@gmail.com>
2021-10-12 16:43:45 -0600

    commit tree 69323314973409d2bf9216e0f6632bba4ea681ab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpBlock){
+    if (pickUpBlock <= 42){
+        return 42 - pickUpBlock
+    } else {
+        return pickUpBlock - 42
+    }
+}
+
+function distanceFromHqInFeet(pickUpBlock) {
+    return distanceFromHqInBlocks(pickUpBlock) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264
+    } else {
+        return (destination - start) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination)
+    if (distance < 400) {
+        return 0
+    } else if (distance <= 2000) {
+        return .02 * (distance - 400)
+    } else if (distance < 2500) {
+        return 25
+    } else {
+        return `cannot travel that far`
+    }
+}
